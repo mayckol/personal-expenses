667a2cb972ec559fc77d395b3e7b82dda89be6eb
mayckol <mayckoll@live.com>
2020-04-21 15:30:44 -0300

    exemplo de ListView usando children

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -12,52 +12,51 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: SingleChildScrollView(
-        child: Column(
-          children: transactions
-              .map((tx) => Card(
-                    child: Row(
-                      children: <Widget>[
-                        Container(
-                          margin:
-                              EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(6),
-                            border: Border.all(
-                              color: Colors.purple,
-                              width: 2,
-                            ),
+      child: ListView(
+        children: transactions
+            .map((tx) => Card(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        margin:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(6),
+                          border: Border.all(
+                            color: Colors.purple,
+                            width: 2,
                           ),
-                          child: Text(
-                            'R\$${tx.amount}',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20,
-                                color: Colors.purple),
-                          ),
-                          padding: EdgeInsets.all(10),
                         ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              tx.title,
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16,
-                              ),
+                        child: Text(
+                          'R\$${tx.amount}',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.purple),
+                        ),
+                        padding: EdgeInsets.all(10),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            tx.title,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
                             ),
-                            Text(
-                              DateFormat.yMMMd().format(tx.date),
-                              style: TextStyle(color: Colors.indigo, fontSize: 12),
-                            ),
-                          ],
-                        )
-                      ],
-                    ),
-                  ))
-              .toList(),
-        ),
+                          ),
+                          Text(
+                            DateFormat.yMMMd().format(tx.date),
+                            style:
+                                TextStyle(color: Colors.indigo, fontSize: 12),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ))
+            .toList(),
       ),
     );
   }
