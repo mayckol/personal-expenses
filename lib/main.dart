3c95e14d6e252daf129c12cd9858450c3b9298e8
mayckol <mayckoll@live.com>
2020-04-21 11:27:11 -0300

    exemplo de interpolacao

--- lib/main.dart
+++ lib/main.dart
@@ -58,7 +58,7 @@ class MyHomePage extends StatelessWidget {
                               ),
                             ),
                             child: Text(
-                              tx.amount.toString().replaceAll('.', ','),
+                              'R\$${tx.amount}',
                               style: TextStyle(
                                   fontWeight: FontWeight.bold,
                                   fontSize: 20,
