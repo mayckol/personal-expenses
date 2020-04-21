b94fd33e8c3986bef22bbdd64a4ef94a8708f798
mayckol <mayckoll@live.com>
2020-04-21 16:08:26 -0300

    metodo submitData

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -7,6 +7,20 @@ class NewTransaction extends StatelessWidget {
 
   NewTransaction(this.addNewTransaction);
 
+  void submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+
+    addNewTransaction(
+      enteredTitle,
+      enteredAmount,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -19,20 +33,20 @@ class NewTransaction extends StatelessWidget {
             TextField(
               controller: titleController,
               decoration: InputDecoration(labelText: 'Title'),
+              onSubmitted: (_) => submitData(),
 //                    onChanged: (value) => titleInput = value,
             ),
             TextField(
               controller: amountController,
               decoration: InputDecoration(labelText: 'Amount'),
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => submitData(),
 //                    onChanged: (value) => amountInput = value,
             ),
             FlatButton(
               child: Text('Add Transaction'),
               textColor: Colors.purple,
-              onPressed: () => addNewTransaction(
-                titleController.text,
-                double.parse(amountController.text),
-              ),
+              onPressed: submitData,
             )
           ],
         ),
