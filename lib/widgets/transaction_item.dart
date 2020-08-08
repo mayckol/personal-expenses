ad64e0904a4c4b7b52ffb56e40658fb8a09c29e9
mayckol <mayckoll@live.com>
2020-08-08 20:03:40 -0300

    separa transaction item do list builder

---
+++ lib/widgets/transaction_item.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:personal_expenses/models/transaction.dart';
+import 'package:intl/intl.dart';
+
+class TransactionItem extends StatelessWidget {
+  final Transaction tr;
+  final Function(String) removeTransaction;
+
+  TransactionItem({
+    this.tr,
+    this.removeTransaction,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+      child: ListTile(
+        leading: CircleAvatar(
+          radius: 30,
+          child: Padding(
+            padding: const EdgeInsets.all(6.0),
+            child: FittedBox(
+              child: Text('R\$${tr.amount}'),
+            ),
+          ),
+        ),
+        title: Text(tr.title, style: Theme.of(context).textTheme.headline6),
+        subtitle: Text(
+          DateFormat('d MMM y').format(tr.date),
+        ),
+        trailing: IconButton(
+          icon: Icon(Icons.delete),
+          onPressed: () => removeTransaction(tr.id),
+          color: Theme.of(context).errorColor,
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
+import 'transaction_item.dart';
 import '../models/transaction.dart';
-import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
-  final void Function(String) removeTransaction;
+  final void Function(String) onRemove;
 
-  TransactionList(this.transactions, this.removeTransaction);
+  TransactionList(this.transactions, this.onRemove);
 
   @override
   Widget build(BuildContext context) {
@@ -40,30 +40,9 @@ class TransactionList extends StatelessWidget {
               itemCount: transactions.length,
               itemBuilder: (ctx, index) {
                 final tr = transactions[index];
-                return Card(
-                  elevation: 5,
-                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: Padding(
-                        padding: const EdgeInsets.all(6.0),
-                        child: FittedBox(
-                          child: Text('R\$${tr.amount}'),
-                        ),
-                      ),
-                    ),
-                    title: Text(tr.title,
-                        style: Theme.of(context).textTheme.headline6),
-                    subtitle: Text(
-                      DateFormat('d MMM y').format(tr.date),
-                    ),
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete),
-                      onPressed: () => removeTransaction(tr.id),
-                      color: Theme.of(context).errorColor,
-                    ),
-                  ),
+                return TransactionItem(
+                  removeTransaction: onRemove,
+                  tr: tr,
                 );
               },
             ),
